1c7db42d1234d265b49ce5a6f4b839241a62ca16
Ami Rahav <arahav@boundlessgeo.com>
2016-11-13 22:32:21 -0500

    Postgres 9.6; GDAL deps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 version=`rpm -qa \*-release | grep -Ei "redhat|centos" | cut -d"-" -f3`
 if [ $version == 7 ];then
+echo "[boundlessps]
+name=boundlessps
+baseurl=https://yum.boundlessps.com/el7/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.boundlessps.com/RPM-GPG-KEY-yum.boundlessps.com
+" > /etc/yum.repos.d/boundlessps.repo
+  rpm -ivh https://yum.postgresql.org/9.6/redhat/rhel-7-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+  rpm -ivh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
   sudo yum -y update
-  rpm -ivh http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm
   sudo yum -y install ant \
                  autoconf \
                  bzip2-devel \
@@ -37,20 +45,32 @@ if [ $version == 7 ];then
                  openssl-devel \
                  openldap-devel \
                  poppler-devel \
-                 postgresql95-devel \
+                 postgresql96-devel \
                  proj-devel \
                  readline-devel \
                  rpmdevtools \
                  sqlite-devel \
-                 swig \
+                 swig-1.3.40 \
                  tk-devel \
                  unzip \
                  xerces-c-devel \
                  zip \
-                 zlib-devel
+                 zlib-devel \
+                 zlib-devel \
+                 hdf5-devel \
+                 netcdf-devel
 
 else
-  curl -o /etc/yum.repos.d/exchange.repo https://yum.boundlessps.com/geoshape.repo
+echo "[boundlessps]
+name=boundlessps
+baseurl=https://yum.boundlessps.com/el6/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.boundlessps.com/RPM-GPG-KEY-yum.boundlessps.com
+" > /etc/yum.repos.d/boundlessps.repo
+
+  rpm -ivh https://yum.postgresql.org/9.6/redhat/rhel-6-x86_64/pgdg-centos96-9.6-3.noarch.rpm
+  rpm -ivh https://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
   sudo yum -y update
   sudo yum -y install ant \
                  autoconf \
@@ -86,7 +106,7 @@ else
                  openssl-devel \
                  openldap-devel \
                  poppler-devel \
-                 postgresql95-devel \
+                 postgresql96-devel \
                  proj-devel \
                  readline-devel \
                  rpmdevtools \
@@ -96,7 +116,9 @@ else
                  unzip \
                  xerces-c-devel \
                  zip \
-                 zlib-devel
+                 zlib-devel \
+                 hdf5-devel \
+                 netcdf-devel
 fi
 
 pushd /vagrant/SOURCES
